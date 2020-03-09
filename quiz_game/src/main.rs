f5ac86c909d3f80ff98f13200e1c2744c503c5c7
Evan <EvanLDouglass@gmail.com>
2020-03-09 13:44:32 -0700

    Take cmdline arg to set csv file

--- quiz_game/src/main.rs
+++ quiz_game/src/main.rs
@@ -1,14 +1,40 @@
+use clap::{App, Arg};
 use std::io;
 use std::process;
 
 use quiz_game::{QAPair, Quiz};
 
 fn main() {
-    let quiz = Quiz::from_csv("./problems.csv").unwrap_or_else(|e| {
-        println!("Problem opening csv: {}", e);
+    // Define command line args
+    let matches = App::new("Quiz Runner")
+        .version("0.1.0")
+        .author("Evan <evanldouglass@gmail.com")
+        .about("Runs a quiz, parsed from a csv, on the command line.")
+        .arg(
+            Arg::with_name("csv_file")
+                .short("c")
+                .long("csv")
+                .takes_value(true)
+                .help(
+                    "A csv file with lines in the form question,answer. \
+                    Must have the header question,answer.",
+                ),
+        )
+        .get_matches();
+
+    let path_str = matches.value_of("csv_file").unwrap_or("./problems.csv");
+
+    // Make quiz
+    let quiz = Quiz::from_csv(path_str).unwrap_or_else(|e| {
+        println!("Problem opening csv {}: {}", path_str, e);
         process::exit(1);
     });
 
+    // Let user start when ready
+    println!("Press Enter when ready");
+    let _ = io::stdin().read_line(&mut String::new());
+
+    // Start quiz
     let mut correct = 0;
     for (index, QAPair { question, answer }) in quiz.question_list().iter().enumerate() {
         // Ask question
@@ -19,10 +45,12 @@ fn main() {
             println!("Problem reading answer, try again.");
         }
 
-        if answer.to_lowercase() == attempt.trim().to_lowercase() {
+        // Check if correct
+        if answer.trim().to_lowercase() == attempt.trim().to_lowercase() {
             correct += 1;
         }
     }
 
+    // Display stats
     println!("\nYou scored {}/{}", correct, quiz.question_list().len());
 }
